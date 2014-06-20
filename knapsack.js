95a017f3520187cdb33e138cc2e63ef6fd8c9ea7
Robert Soto <robsoto@mit.edu>
2014-06-20 01:16:21 -0400

    Added animations

--- knapsack.js
+++ knapsack.js
@@ -1,11 +1,10 @@
 /*
 	Things to add:
-		WORKING RESET BUTTON W/ SFX
+		keep previous state on page refresh
 		pie chart that fills as items are added
 		display results of previous attempts
 		drag and drop
 		multiple select
-		change cursor when items are moused over 
 		animate transitions
 */
 
@@ -25,28 +24,41 @@ function moveItem($elem) {
 		if (validWeight($elem)) {
 			moveSound.play();
 			$elem.attr('data-location', 'knapsack');
-			$('#knapsack').append($elem);
+			$elem.toggle('puff', {percent:110}, function() {
+				$('#knapsack').append($elem);	
+			});
+			$elem.toggle('puff', {percent:110});
 			updateKnapsack($elem);
 		}
 		else {
 			$elem.effect('shake');
 			errorSound.play();
+			$('#error').text("That's more than I can carry!")
+					   .css('color', 'red')
+					   .css('font-size', 'large')
+					   .delay(750)
+					   .fadeOut(1000, function() {
+							   	$('#error').text('');
+						})
+					   .fadeIn();
 		}
 	}
 	//knapsack to house
 	else {
 		moveSound.play();
 		$elem.attr('data-location', 'house');
-		$('#house').append($elem);
+		$elem.toggle('puff', {percent:110}, function() {
+			$('#house').append($elem);	
+		});
+		$elem.toggle('puff', {percent:110});
 		updateKnapsack($elem);
 	}
 }
 
 //display the value and weight of each item
 function displayValAndWeight() {
-	items = $('.item');
-	images = items.find('figcaption');
-	console.log(images);
+	var items = $('.item');
+	var images = items.find('figcaption');
 	items.each(function() {
 		var info = '$' + $(this).find('img').attr('data-value') + ', ' + $(this).find('img').attr('data-weight') + ' kg';
 		$(this).find('figcaption').text(info);
@@ -55,7 +67,7 @@ function displayValAndWeight() {
 
 //display the value of each item as $/kg
 function displayValPerWeight() {
-	items = $('.item');
+	var items = $('.item');
 	items.each(function() {
 		var val = parseFloat($(this).find('img').attr('data-value'));
 		var weight = parseFloat($(this).find('img').attr('data-weight'));
@@ -81,13 +93,23 @@ function updateKnapsack($elem) {
 	$('#knapsackContents').text('Value: $' + knapsackValue + '\n Weight: ' + knapsackWeight + ' kg' + '\n(Limit: ' + weightLimit + ' kg)');
 }
 
+//moves all items back into house
 function reset() {
-	items = $('.item');
+    var resetSound = new Audio('resetSound.mp3');
+	if ($('#knapsack > div').length > 0) {
+		resetSound.play();
+	}
+	
+	var items = $('.item');
 	items.each(function() {
-		if ($(this).attr('data-location') == 'knapsack') {
-			updateKnapsack($(this));
-			$(this).find('div').attr('data-location', 'house');
-			$('#house').append($(this));
+		var item = $(this)
+		if (item.attr('data-location') == 'knapsack') {
+			item.attr('data-location', 'house');
+			item.toggle('puff', {percent:110}, function() {
+				$('#house').append(item);	
+			});
+			item.toggle('puff', {percent:110})
+			updateKnapsack(item);
 		}
 	});
 }
@@ -105,21 +127,26 @@ $(document).ready(function() {
 	$('#knapsackContents').text('Value: $' + 0 + '\n Weight: ' + 0 + ' kg' + '\n(Limit: ' + weightLimit + ' kg)');
 	displayValAndWeight();
 	
+	//change cursor on hover
+	items.find('img').hover(function() {
+		$(this).css('cursor', 'pointer');
+	});
+	
 	//move items on click
-	items.click(function(e) {
+	items.click(function() {
 		moveItem($(this));
 	});
 	
 	//button click actions
-	buttons.click(function(e) {
+	buttons.click(function() {
 		if ($(this).attr('id') == 'valAndWeight') {
 			displayValAndWeight();	
 		}
 		else if ($(this).attr('id') == 'valPerWeight') {
 			displayValPerWeight();
 		}
-		/*else if ($(this).attr('id') == 'reset') { 
+		else if ($(this).attr('id') == 'reset') { 
 			reset();
-		}*/
+		}
 	});
 });
